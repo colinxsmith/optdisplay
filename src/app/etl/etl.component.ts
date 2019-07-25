921445076e3f2fdc274ee31d515f23fd0a2a2fe1
Colin <colinxsmith@gmail.com>
2019-07-25 23:39:33 +0100

    input at bottom works

--- src/app/etl/etl.component.ts
+++ src/app/etl/etl.component.ts
@@ -16,30 +16,31 @@ export class EtlComponent implements OnInit {
   constructor(private mainScreen: ElementRef) { }
 
   ngOnInit() {
+    this.stockNames = [];
+    this.stockLower = [];
+    this.stockUpper = [];
     this.chooser();
   }
   chooser() {
     d3.select(this.mainScreen.nativeElement).select('#stockdata').selectAll('div').remove();
-    this.stockNames = [];
-    this.stockLower = [];
-    this.stockUpper = [];
-    for (let i = 0; i < 19; i++) {
-      this.stockNames.push(`stock${i}`);
-      this.stockLower.push(0);
-      this.stockUpper.push(1);
+    if (this.stockNames.length === 0) {
+      for (let i = 0; i < 19; i++) {
+        this.stockNames.push(`stock${i}`);
+        this.stockLower.push(0);
+        this.stockUpper.push(1);
+      }
     }
     const tableFormat = (i: number | string) =>
       isString(i as string) ? i as string : d3.format('0.5f')(i as number);
-    const ww = 900, xPos = d3.scaleLinear().domain([0, 3]).range([0, ww]);
+    const ww = 900, xPos = d3.scaleLinear().domain([0, 4]).range([0, ww]);
     const hh = 20 * this.stockNames.length, yPos = d3.scaleLinear().domain([0, this.stockNames.length]).range([0, hh]);
     d3.select(this.mainScreen.nativeElement).select('#stockdata').append('div').attr('class', 'inputData')
       .append('svg')
       .attr('width', ww)
-      .attr('height', hh).append('g')
+      .attr('height', hh)
       .selectAll('.bounds')
       .data(this.stockNames).enter()
-      .append('g')
-      .append('text').text(d => d)
+      .append('text')
       .call(d => d.each((dd, i, j) => {
         const here = d3.select(j[i]);
         for (let kk = 0; kk < 3; ++kk) {
@@ -48,11 +49,13 @@ export class EtlComponent implements OnInit {
             out = this.stockLower[i];
           } else if (kk === 2) {
             out = this.stockUpper[i];
+          } else if (kk === 0) {
+            out = this.stockNames[i];
           }
           const t = (kk + 1) / 3;
           here.append('tspan')
-            .attr('x', xPos(kk))
-            .attr('y', yPos(i))
+            .attr('x', xPos(kk + 1))
+            .attr('y', yPos(i + 1))
             .attr('class', 'spacer')
             .style('fill', `${d3.rgb(200 * (1 - t), t / 2 * 255, 200 * t)}`)
             .text(tableFormat(out));
@@ -60,13 +63,26 @@ export class EtlComponent implements OnInit {
       }));
     d3.selectAll('tspan')
       .on('click', (d, i, j) => {
+        const id = i % 3;
+        const stock = Math.floor(i / 3);
+        console.log(i, id, stock);
         const here = (j[i] as SVGTSpanElement);
-        const field = d3.select(this.mainScreen.nativeElement).select('#stockdata').select('div').append('input');
+        const field = d3.select(this.mainScreen.nativeElement).select('#stockdata').append('input');
+          field.attr('x', here.getAttribute('x'));
+          field.attr('y', here.getAttribute('y'));
         field.text(here.textContent);
-        field.on('change', (d, i, j) => {
-          const id = i % 3;
-          const stock = Math.floor(i % this.stockNames.length);
-          if (id === 1) { this.stockLower[stock] = +j[i].value; }
+        field.on('change', (dd, ii, jj) => {
+          const val = +jj[ii].value;
+          console.log(val);
+          if (id === 1) {
+            this.stockLower[stock] = val;
+            here.textContent = `${val}`;
+          } else if (id === 2) {
+            this.stockUpper[stock] = val;
+            here.textContent = `${val}`;
+          }
+          this.chooser();
+          field.remove();
         });
       });
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  serverUrl: 'http://192.168.0.27:3000'
+  serverUrl: 'http://192.168.0.21:3000'
 };
