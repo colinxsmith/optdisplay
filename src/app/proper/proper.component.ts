3ae7514062c1ace886759cad0a030a6474eaf1d9
Colin <colinxsmith@gmail.com>
2019-08-28 23:59:36 +0100

    good demo

--- src/app/proper/proper.component.ts
+++ src/app/proper/proper.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ElementRef, ViewEncapsulation } from '@angular/core';
 import { DataService } from '../data.service';
 import * as d3 from 'd3';
-
+import { select } from 'd3';
 @Component({
   selector: 'app-proper',
   templateUrl: './proper.component.html',
@@ -9,36 +9,37 @@ import * as d3 from 'd3';
   encapsulation: ViewEncapsulation.None
 })
 export class ProperComponent implements OnInit {
-  DATA = [{ x: 1, name: 'name1' }, { x: -2, name: 'name2' }, { x: 3, name: 'name3' }];
+  DATA: { x: number }[];
   w: number;
   h: number;
   scaleX: d3.ScaleLinear<number, number>;
   constructor(private mainElement: ElementRef) { }
-
   ngOnInit() {
+    this.DATA = [];
+    for (let i = 0; i < 10; ++i) {
+      this.DATA.push({ x: ((i + 1) - 5.5) * ((i + 1) - 4.5) * ((i + 1) - 3.5) });
+    }
     const SVG = d3.select(this.mainElement.nativeElement).select('#barchart');
-    console.log(SVG.attr('width'), SVG.attr('height'));
     this.w = +SVG.attr('width');
     this.h = +SVG.attr('height');
     this.scaleX = d3.scaleLinear().domain([d3.min(this.DATA.map(d => d.x)), d3.max(this.DATA.map(d => d.x))]).range([0, this.w]);
-    const BASE = SVG.selectAll('.bars').data(this.DATA).enter().append('g');
+    SVG.selectAll('.bars').data(this.DATA).enter().append('g');
     const BARS: d3.Selection<SVGGElement, {
       x: number;
-      name: string;
     }, d3.BaseType, unknown>
-      = d3.select(this.mainElement.nativeElement).select('svg').selectAll('g');
-
+      = d3.select(this.mainElement.nativeElement).select('#barchart').selectAll('g');
     BARS.append('rect')
       .attr('x', d => d.x > 0 ? this.scaleX(0) : this.scaleX(d.x))
       .attr('width', d => d.x < 0 ? this.scaleX(0) - this.scaleX(d.x) : this.scaleX(d.x) - this.scaleX(0))
       .attr('y', (d, i) => this.h * i / this.DATA.length)
-      .attr('height', this.h / this.DATA.length);
-
+      .attr('height', this.h / this.DATA.length)
+      .on('click', (d, i, j) => {
+        j[i].setAttribute('style', 'fill:' + (d.x > 0 ? 'blue' : 'orange'));
+      });
   }
-  clicked(DA: { x: number, names: string }, i: number) {
-    const PROPERBARs = d3.select(this.mainElement.nativeElement).select('#proper').selectAll('rect');
-    PROPERBARs
-      .style('fill', (d, ii) => i === ii ? (DA.x > 0 ? 'blue' : 'red') : '');
+  clicked(DA: { x: number }, i: number) {
+    const BARS = d3.select(this.mainElement.nativeElement).select('#proper').selectAll('rect');
+    (BARS.nodes()[i] as SVGRectElement)
+      .setAttribute('style', 'fill:' + (DA.x > 0 ? 'blue' : 'red'));
   }
-
 }
