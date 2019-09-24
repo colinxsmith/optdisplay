b1a21ab7370c0ab200a6d7272529d858fd1f36ea
Colin <colinxsmith@gmail.com>
2019-09-24 11:02:07 +0100

    forgot

--- src/app/bulktrade/bulktrade.component.ts
+++ src/app/bulktrade/bulktrade.component.ts
@@ -148,7 +148,7 @@ export class BulktradeComponent implements OnInit, AfterViewInit {
       }) => {
         this.toolTipObj.attr('style', `left:${d3.event.pageX + 20}px;top:${d3.event.pageY + 20}px;display:inline-block`)
           .html(`${this.DATA.label}<br>${d.outlierStatusType}<br>${d.value}`);
-      })
+      });
        PATHS .on('mouseout', () => {
           this.toolTipObj.attr('style', `display:none`)
             .html('');
