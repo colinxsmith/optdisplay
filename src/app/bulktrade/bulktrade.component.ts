e32aae82f9c25cbc5aa7a6df61440059a7f1c9b1
Colin Smith <colin.smith@corfinancialgroup.com>
2019-10-22 16:16:36 +0100

    padangle for squarearc

--- src/app/bulktrade/bulktrade.component.ts
+++ src/app/bulktrade/bulktrade.component.ts
@@ -33,6 +33,8 @@ export class BulktradeComponent implements OnInit, OnChanges {
   rimAnagle = 0.08 * Math.PI * 2;
   scaleArc = d3.scaleLinear();
   fontSize: number;
+  eps = Math.abs((4 / 3 - 1) * 3 - 1);
+  @Input() square = true;
   @Input() toolTipObj = d3.select('app-root').select('div.mainTip');
   @Input() width = 800;
   @Input() height = 800;
@@ -97,31 +99,25 @@ export class BulktradeComponent implements OnInit, OnChanges {
     this.scaleArc.domain([0, totalV]).range([Math.PI + this.rimAnagle, 3 * Math.PI - this.rimAnagle]);
   }
   arcPath(i: number, t = 1) {
-    //    console.log(i, t);
     let sofar = 0;
     for (let ii = 0; ii < i; ++ii) {
       sofar += this.DATA.monitorFlagCategory[ii].value;
     }
-    const ARC = d3.arc().cornerRadius(10);
-    //    console.log(sofar, sofar + this.DATA.monitorFlagCategory[i].value);
-    //    console.log(this.scaleArc(sofar), this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value));
+    const ARC = this.square ? this.squareArc : d3.arc().cornerRadius(10);
     return ARC({
       innerRadius: this.side / 2 * 0.7 * t, outerRadius: t * this.side / 2 * 0.8, startAngle: this.scaleArc(sofar)
-      , endAngle: t * t * this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value), padAngle: 1 - t * t + 0.01
+      , endAngle: t * t * this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value), padAngle: t * t * 0.01
     });
   }
   centroid(i: number, t = 1) {
-    //    console.log(i, t);
     let sofar = 0;
     for (let ii = 0; ii < i; ++ii) {
       sofar += this.DATA.monitorFlagCategory[ii].value;
     }
     const ARC = d3.arc().cornerRadius(10);
-    //    console.log(sofar, sofar + this.DATA.monitorFlagCategory[i].value);
-    //    console.log(this.scaleArc(sofar), this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value));
     return ARC.centroid({
       innerRadius: this.side / 2 * 0.7 * t, outerRadius: t * this.side / 2 * 0.8, startAngle: this.scaleArc(sofar)
-      , endAngle: t * t * this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value), padAngle: 1 - t * t + 0.01
+      , endAngle: t * t * this.scaleArc(sofar + this.DATA.monitorFlagCategory[i].value), padAngle: 0
     });
   }
   translateHack(w: number, h: number) {
@@ -159,5 +155,284 @@ export class BulktradeComponent implements OnInit, OnChanges {
         .style('font-size', fontSize + 'px')
         .attr('transform', (d, i) => `translate(${this.side / 2},${this.side / 2 + fontSize * 2 * (i - 1)})`);
     }
+  }
+  squareArc = (parm: { outerRadius: number, innerRadius: number, startAngle: number, endAngle: number, padAngle: number }) => {
+    if (parm.endAngle - parm.startAngle <= parm.padAngle) {
+      parm.padAngle = 0;
+    }
+    parm.startAngle -= Math.PI * 0.5 - parm.padAngle;
+    parm.endAngle -= Math.PI * 0.5 + parm.padAngle;
+    const makeZ = (x: number) => Math.abs(x) < this.eps ? 0 : x;
+    const seg1 = { xx1: 0, xx2: 0, yy1: 0, yy2: 0 };
+    const seg2 = { xx1: 0, xx2: 0, yy1: 0, yy2: 0, face: 0 };
+    if (parm.innerRadius === 0) {
+      parm.innerRadius = this.eps;
+    }
+    if (parm.outerRadius === 0) {
+      parm.outerRadius = this.eps;
+    }
+    seg1.xx1 = parm.innerRadius * Math.cos(parm.startAngle);
+    seg1.yy1 = parm.innerRadius * Math.sin(parm.startAngle);
+    if (Math.abs(seg1.xx1) > Math.abs(seg1.yy1)) {
+      seg1.yy1 *= Math.abs(parm.innerRadius / seg1.xx1);
+      seg1.xx1 = seg1.xx1 < 0 ? -parm.innerRadius : parm.innerRadius;
+    } else {
+      seg1.xx1 *= Math.abs(parm.innerRadius / seg1.yy1);
+      seg1.yy1 = seg1.yy1 < 0 ? -parm.innerRadius : parm.innerRadius;
+    }
+    seg1.xx2 = parm.outerRadius * Math.cos(parm.startAngle);
+    seg1.yy2 = parm.outerRadius * Math.sin(parm.startAngle);
+    if (Math.abs(seg1.xx2) > Math.abs(seg1.yy2)) {
+      seg1.yy2 *= Math.abs(parm.outerRadius / seg1.xx2);
+      seg1.xx2 = seg1.xx2 < 0 ? -parm.outerRadius : parm.outerRadius;
+    } else {
+      seg1.xx2 *= Math.abs(parm.outerRadius / seg1.yy2);
+      seg1.yy2 = seg1.yy2 < 0 ? -parm.outerRadius : parm.outerRadius;
+    }
+    seg2.xx1 = parm.innerRadius * Math.cos(parm.endAngle);
+    seg2.yy1 = parm.innerRadius * Math.sin(parm.endAngle);
+    if (Math.abs(seg2.xx1) > Math.abs(seg2.yy1)) {
+      seg2.yy1 *= Math.abs(parm.innerRadius / seg2.xx1);
+      seg2.xx1 = seg2.xx1 < 0 ? -parm.innerRadius : parm.innerRadius;
+    } else {
+      seg2.xx1 *= Math.abs(parm.innerRadius / seg2.yy1);
+      seg2.yy1 = seg2.yy1 < 0 ? -parm.innerRadius : parm.innerRadius;
+    }
+    seg2.xx2 = parm.outerRadius * Math.cos(parm.endAngle);
+    seg2.yy2 = parm.outerRadius * Math.sin(parm.endAngle);
+    if (Math.abs(seg2.xx2) > Math.abs(seg2.yy2)) {
+      seg2.yy2 *= Math.abs(parm.outerRadius / seg2.xx2);
+      seg2.xx2 = seg2.xx2 < 0 ? -parm.outerRadius : parm.outerRadius;
+    } else {
+      seg2.xx2 *= Math.abs(parm.outerRadius / seg2.yy2);
+      seg2.yy2 = seg2.yy2 < 0 ? -parm.outerRadius : parm.outerRadius;
+    }
+    if (seg1.xx1 === -parm.innerRadius && seg2.xx1 === -parm.innerRadius) {
+      // both left side
+      if (seg2.yy1 <= seg1.yy1) {
+        seg2.face = 0;
+      } else {
+        seg2.face = 4;
+      }
+    } else if (seg1.yy1 === -parm.innerRadius && seg2.yy1 === -parm.innerRadius) {
+      // both top side
+      if (seg2.xx1 >= seg1.xx1) {
+        seg2.face = 0;
+      } else {
+        seg2.face = 4;
+      }
+    } else if (seg1.xx1 === parm.innerRadius && seg2.xx1 === parm.innerRadius) {
+      // both right side
+      if (seg2.yy1 >= seg1.yy1) {
+        seg2.face = 0;
+      } else {
+        seg2.face = 4;
+      }
+    } else if (seg1.yy1 === parm.innerRadius && seg2.yy1 === parm.innerRadius) {
+      // both bottom side
+      if (seg2.xx1 <= seg1.xx1) {
+        seg2.face = 0;
+      } else {
+        seg2.face = 4;
+      }
+    } else if (seg1.xx1 === -parm.innerRadius && seg2.yy1 === -parm.innerRadius) {
+      // left to top
+      seg2.face = 1;
+    } else if (seg1.xx1 === -parm.innerRadius && seg2.xx1 === parm.innerRadius) {
+      // left to right
+      seg2.face = 2;
+    } else if (seg1.xx1 === -parm.innerRadius && seg2.yy1 === parm.innerRadius) {
+      // left to bottom
+      seg2.face = 3;
+    } else if (seg1.yy1 === -parm.innerRadius && seg2.xx1 === parm.innerRadius) {
+      // top to right
+      seg2.face = 1;
+    } else if (seg1.yy1 === -parm.innerRadius && seg2.yy1 === parm.innerRadius) {
+      // top to bottom
+      seg2.face = 2;
+    } else if (seg1.yy1 === -parm.innerRadius && seg2.xx1 === -parm.innerRadius) {
+      // top to left
+      seg2.face = 3;
+    } else if (seg1.xx1 === parm.innerRadius && seg2.yy1 === parm.innerRadius) {
+      // right to bottom
+      seg2.face = 1;
+    } else if (seg1.xx1 === parm.innerRadius && seg2.xx1 === -parm.innerRadius) {
+      // right to left
+      seg2.face = 2;
+    } else if (seg1.xx1 === parm.innerRadius && seg2.yy1 === -parm.innerRadius) {
+      // right to top
+      seg2.face = 3;
+    } else if (seg1.yy1 === parm.innerRadius && seg2.xx1 === -parm.innerRadius) {
+      // bottom to left
+      seg2.face = 1;
+    } else if (seg1.yy1 === parm.innerRadius && seg2.yy1 === -parm.innerRadius) {
+      // bottom to top
+      seg2.face = 2;
+    } else if (seg1.yy1 === parm.innerRadius && seg2.xx1 === parm.innerRadius) {
+      // bottom to right
+      seg2.face = 3;
+    }
+    let quadR = 'M ' + seg1.xx2 + ' ' + seg1.yy2 + ' L ' + seg1.xx1 + ' ' + seg1.yy1;
+    seg1.xx1 = makeZ(seg1.xx1);
+    seg1.yy1 = makeZ(seg1.yy1);
+    seg1.xx2 = makeZ(seg1.xx2);
+    seg1.yy2 = makeZ(seg1.yy2);
+    seg2.xx1 = makeZ(seg2.xx1);
+    seg2.yy1 = makeZ(seg2.yy1);
+    seg2.xx2 = makeZ(seg2.xx2);
+    seg2.yy2 = makeZ(seg2.yy2);
+    if (seg2.face === 0) {
+      if (seg1.xx1 === -parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + -parm.outerRadius + ' ' + seg2.yy2;
+      } else if (seg1.xx1 === parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + parm.outerRadius + ' ' + seg2.yy2;
+      } else if (seg1.yy1 === -parm.innerRadius) {
+        quadR += 'L ' + seg2.xx1 + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + -parm.outerRadius;
+      } else if (seg1.yy1 === parm.innerRadius) {
+        quadR += 'L ' + seg2.xx1 + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + parm.outerRadius;
+      }
+    } else if (seg2.face === 1) {
+      if (seg1.xx1 === -parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.xx1 === parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+      } else if (seg1.yy1 === -parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.yy1 === parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + -parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+      }
+    } else if (seg2.face === 2) {
+      if (seg1.xx1 === -parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.xx1 === parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + -parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+      } else if (seg1.yy1 === -parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.yy1 === parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+      }
+    } else if (seg2.face === 3) {
+      if (seg1.xx1 === -parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.xx1 === parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+      } else if (seg1.yy1 === -parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + -parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.yy1 === parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+      }
+    } else if (seg2.face === 4) {
+      if (seg1.xx1 === -parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + -parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.xx1 === parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + seg2.yy1;
+        quadR += 'L ' + parm.outerRadius + ' ' + seg2.yy2;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+      } else if (seg1.yy1 === -parm.innerRadius) {
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + -parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+      } else if (seg1.yy1 === parm.innerRadius) {
+        quadR += 'L ' + -parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + -parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + -parm.innerRadius;
+        quadR += 'L ' + parm.innerRadius + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx1 + ' ' + parm.innerRadius;
+        quadR += 'L ' + seg2.xx2 + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + parm.outerRadius;
+        quadR += 'L ' + parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + -parm.outerRadius;
+        quadR += 'L ' + -parm.outerRadius + ' ' + parm.outerRadius;
+      }
+    }
+    quadR += 'Z'; // Closed curve
+    return quadR;
   }
 }
