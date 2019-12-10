58263ac6fb39a9372a8a75f59ff4cf247e36ac72
Colin Smith <colin.smith@corfinancialgroup.com>
2019-12-10 11:19:28 +0000

    rotation and positioning

--- src/app/rec/rec.component.ts
+++ src/app/rec/rec.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
 import * as d3 from 'd3';
 
 @Component({
@@ -6,17 +6,20 @@ import * as d3 from 'd3';
   templateUrl: './rec.component.html',
   styleUrls: ['./rec.component.css']
 })
-export class RecComponent implements OnInit {
+export class RecComponent implements OnInit, OnChanges {
   inputText = '';
   constructor() { }
 
   ngOnInit() {
     console.log('init');
   }
-  clicker = () => {
-    this.inputText = 'clicked';
+  ngOnChanges() {
+    console.log('changes');
+  }
+  clicker = (ang: number) => {
+    this.inputText = ang === 0 ? '' : 'clicked';
     const g = d3.select('#RECEIVER').select('g');
-    g.attr('transform', this.translatehack(264.5, 90));
+    g.attr('transform', `translate(400,400) rotate(${ang})`);
   }
   translatehack = (R: number, r: number) => `translate(${R * Math.sin(r / 180 * Math.PI)},${R * Math.cos(r / 180 * Math.PI)}) rotate(${r})`;
   update() {
