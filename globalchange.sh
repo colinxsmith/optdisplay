923d2ab85534c7b389f89e9cd839343ba842eb4a
Colin <colinxsmith@gmail.com>
2020-01-12 17:25:02 +0000

    we meant this

--- globalchange.sh
+++ globalchange.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-for file in src/* src/app/* src/app/*/*; do if [ -f src/app/rec/rec.component.ts ]; then sed -i "s/data-title/greentitle/g" src/app/rec/rec.component.ts; fi; done
+for file in src/* src/app/* src/app/*/*; do if [ -f $file ]; then sed -i "s/data-title/greentitle/g" $file; fi; done
