3f36e5548f1de79ac981519ae7e0dbda9a8198ca
Colin Smith <Colin.Smith@corfinancialgroup.com>
2020-03-16 23:45:57 +0000

    colour setup

--- src/app/usedart/usedart.component.ts
+++ src/app/usedart/usedart.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as d3 from 'd3';
+import { CompileShallowModuleMetadata } from '@angular/compiler';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-usedart',
@@ -10,15 +12,14 @@ export class UsedartComponent implements OnInit {
   scale = 1000;
   scl = 0.01;
   colourScale: d3.ScaleLinear<d3.RGBColor, string>;
-  topcolour: string;
-  bottomcolour: string;
+  topcolour = 'red';
+  bottomcolour = 'rgb(200,100,50)';
   received = 5;
   colourgamma = 0.5;
   constructor() { }
 
   ngOnInit() {
-    this.topcolour = 'red';
-    this.bottomcolour = 'blue';
+    this.colourgamma = +(d3.select('#slide').node() as HTMLInputElement).value / 1000;
     this.colourScale = d3.scaleLinear<d3.RGBColor>()
       .domain([0, 10])
       .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
@@ -30,5 +31,21 @@ export class UsedartComponent implements OnInit {
   }
   newgamma(ev: MouseEvent) {
     this.colourgamma = +(ev.target as HTMLInputElement).value / 1000;
+  }
+  getTopcolour(ev: MouseEvent) {
+    this.topcolour = (ev.target as HTMLInputElement).value;
+    this.colourScale = d3.scaleLinear<d3.RGBColor>()
+      .domain([0, 10])
+      .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
+      .range([d3.rgb(this.topcolour), d3.rgb(this.bottomcolour)])
+      ;
+  }
+  getBottomcolour(ev: MouseEvent) {
+    this.bottomcolour = (ev.target as HTMLInputElement).value;
+    this.colourScale = d3.scaleLinear<d3.RGBColor>()
+      .domain([0, 10])
+      .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
+      .range([d3.rgb(this.topcolour), d3.rgb(this.bottomcolour)])
+      ;
   }
 }
