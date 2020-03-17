f05c3e8e8c637443ec6b889a8dbe9ec3c16a3974
Colin Smith <Colin.Smith@corfinancialgroup.com>
2020-03-17 11:34:09 +0000

    keep here

--- src/app/usedart/usedart.component.ts
+++ src/app/usedart/usedart.component.ts
@@ -10,40 +10,41 @@ export class UsedartComponent implements OnInit {
   scale = 1000;
   scl = 0.01;
   colourScale: d3.ScaleLinear<d3.RGBColor, string>;
-  topcolour = 'red';
-  bottomcolour = 'rgb(200,100,50)';
+  topcolour = 'orange';
+  bottomcolour = 'green';
   received = 5;
   colourgamma = 0.5;
   constructor() { }
-
+  formatC = (i: number) => d3.format('0.2f')(i);
   ngOnInit() {
     this.colourgamma = +(d3.select('#slide').node() as HTMLInputElement).value / 1000;
-    this.colourScale = d3.scaleLinear<d3.RGBColor>()
-      .domain([0, 10])
-      .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
-      .range([d3.rgb(this.topcolour), d3.rgb(this.bottomcolour)])
-      ;
+    setTimeout(() => {
+      this.update();
+    });
   }
   newcolour(ev: MouseEvent) {
     this.received = +(ev.target as HTMLInputElement).value * this.scl;
+    d3.select('app-dartboard').style('--back', this.colourScale(this.received));
+    this.update();
   }
   newgamma(ev: MouseEvent) {
     this.colourgamma = +(ev.target as HTMLInputElement).value / 1000;
+    this.update();
   }
   getTopcolour(ev: MouseEvent) {
     this.topcolour = (ev.target as HTMLInputElement).value;
-    this.colourScale = d3.scaleLinear<d3.RGBColor>()
-      .domain([0, 10])
-      .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
-      .range([d3.rgb(this.topcolour), d3.rgb(this.bottomcolour)])
-      ;
+    this.update();
   }
   getBottomcolour(ev: MouseEvent) {
     this.bottomcolour = (ev.target as HTMLInputElement).value;
+    this.update();
+  }
+  update() {
     this.colourScale = d3.scaleLinear<d3.RGBColor>()
       .domain([0, 10])
       .interpolate(d3.interpolateRgb.gamma(this.colourgamma))
       .range([d3.rgb(this.topcolour), d3.rgb(this.bottomcolour)])
       ;
+    d3.select('app-dartboard').style('--back', this.colourScale(this.received));
   }
 }
